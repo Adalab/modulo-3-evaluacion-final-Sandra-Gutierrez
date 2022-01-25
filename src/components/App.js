d15a63a236bff3642861389bbd1835583e66b338
Sandra Gutierrez <sandsan12@gmail.com>
2022-01-25 17:15:59 +0100

    Add filter name function

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,8 @@ console.log("( => Ready! )");
 function App() {
   // Estado
   const [data, setData] = useState([]);
+  const [filterName, setFilterName] = useState("");
+  const [filterHouse, setFilterHouse] = useState("");
   // Api
   useEffect(() => {
     callToApi().then((response) => {
@@ -14,17 +16,27 @@ function App() {
     });
   }, []);
   // Handle Functions
+  const handleChangeFilterName = (ev) => {
+    setFilterName(ev.currentTarget.value);
+  };
+  const handleChangeFilterHouse = () => {};
   // Render Functions
   const renderCharacters = () => {
-    return data.map((character, index) => {
-      return (
-        <li key={index}>
-          <img src={character.image} alt={`Retrato de ${character.name}`} />
-          <p>{character.name}</p>
-          <p>{character.species}</p>
-        </li>
-      );
-    });
+    return data
+      .filter((character) =>
+        character.name
+          .toLocaleLowerCase()
+          .includes(filterName.toLocaleLowerCase())
+      )
+      .map((character, index) => {
+        return (
+          <li key={index}>
+            <img src={character.image} alt={`Retrato de ${character.name}`} />
+            <p>{character.name}</p>
+            <p>{character.species}</p>
+          </li>
+        );
+      });
   };
   // React Render HTML
   return (
@@ -34,7 +46,27 @@ function App() {
       </header>
       <main>
         <section>
-          <form action=""></form>
+          <form className="" action="" onSubmit={(ev) => ev.preventDefault()}>
+            <label htmlFor="">Busca por personaje</label>
+            <input
+              type="text"
+              placeholder="Ej. Sirius  Black"
+              onChange={handleChangeFilterName}
+              value={filterName}
+            />
+            <label htmlFor="house">Selecciona la casa</label>
+            <select
+              name="house"
+              id=""
+              onChange={handleChangeFilterHouse}
+              value={filterHouse}
+            >
+              <option value="Gryffindor">Gryffindor</option>
+              <option value="Slytherin">Slytherin</option>
+              <option value="Ravenclaw">Ravenclaw</option>
+              <option value="Hufflepuff">Hufflepuff</option>
+            </select>
+          </form>
         </section>
         <section>
           <ul>{renderCharacters()}</ul>
